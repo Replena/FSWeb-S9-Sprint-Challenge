7150e10a188e6f6844397623371f214bb31fafb3
Alperen <alperenmimarlar@gmail.com>
2024-09-20 19:58:53 +0300

    Refactor AppFunctional component and test passed

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const initialMessage = "";
 const initialEmail = "";
 const initialSteps = 0;
 const initialIndex = 4;
-const boundaryMessage = "Daha fazla ilerleyemezsiniz.";
-const invalidEmailMessage = "Ouch: email must be a valid email";
 
 export default function AppFunctional(props) {
   const [message, setMessage] = useState(initialMessage);
@@ -13,11 +12,14 @@ export default function AppFunctional(props) {
   const [steps, setSteps] = useState(initialSteps);
   const [index, setIndex] = useState(initialIndex);
 
-  const row = Math.floor(index / 3) + 1;
-  const column = (index % 3) + 1;
+  function getXY() {
+    const xy = { row: Math.floor(index / 3) + 1, column: (index % 3) + 1 };
+    return xy;
+  }
 
   function getXYMesaj() {
-    return `Koordinatlar (${row}, ${column})`;
+    const { row, column } = getXY();
+    return `Koordinatlar (${column}, ${row})`;
   }
 
   function reset() {
@@ -28,24 +30,34 @@ export default function AppFunctional(props) {
   }
 
   function sonrakiIndex(yon) {
-    const movements = {
-      up: row > 1 ? index - 3 : index,
-      down: row < 3 ? index + 3 : index,
-      left: column > 1 ? index - 1 : index,
-      right: column < 3 ? index + 1 : index,
-    };
-    return movements[yon];
+    const { row, column } = getXY();
+    if (yon === "up" && row > 1) {
+      return index - 3;
+    } else if (yon === "up" && row <= 1) {
+      setMessage("Yukarıya gidemezsiniz");
+    } else if (yon === "down" && row < 3) {
+      return index + 3;
+    } else if (yon === "down" && row >= 3) {
+      setMessage("Aşağıya gidemezsiniz");
+    } else if (yon === "left" && column > 1) {
+      return index - 1;
+    } else if (yon === "left" && column <= 1) {
+      setMessage("Sola gidemezsiniz");
+    } else if (yon === "right" && column < 3) {
+      return index + 1;
+    } else if (yon === "right" && column >= 3) {
+      setMessage("Sağa gidemezsiniz");
+    }
+    return index;
   }
 
   function ilerle(evt) {
-    const newIndex = sonrakiIndex(evt.target.id);
-
-    if (newIndex !== index) {
-      setIndex(newIndex);
-      setSteps((prevSteps) => prevSteps + 1);
+    const yon = evt.target.id;
+    const gunceIndex = sonrakiIndex(yon);
+    if (gunceIndex !== index) {
+      setIndex(gunceIndex);
+      setSteps((prevsteps) => prevsteps + 1);
       setMessage(initialMessage);
-    } else {
-      setMessage(boundaryMessage);
     }
   }
 
@@ -54,13 +66,26 @@ export default function AppFunctional(props) {
   }
 
   function onSubmit(evt) {
+    const { row, column } = getXY();
     evt.preventDefault();
-    if (email === "" || !email.includes("@")) {
-      setMessage(invalidEmailMessage);
-    } else {
-      setMessage(`Başarıyla gönderildi: ${email}`);
-      setEmail(initialEmail);
-    }
+
+    axios
+      .post("http://localhost:9000/api/result", {
+        x: column,
+        y: row,
+        steps: steps,
+        email: email,
+      })
+      .then(function (response) {
+        console.log(response);
+        setMessage(response.data.message);
+      })
+      .catch(function (error) {
+        console.log(error);
+        console.log(error.response.message);
+        setMessage(error.response.data.message);
+      });
+    setEmail(initialEmail);
   }
 
   return (
@@ -71,7 +96,6 @@ export default function AppFunctional(props) {
         </h3>
         <h3 id="steps">{steps} kere ilerlediniz</h3>
       </div>
-
       <div id="grid">
         {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
           <div key={idx} className={`square${idx === index ? " active" : ""}`}>
@@ -79,11 +103,9 @@ export default function AppFunctional(props) {
           </div>
         ))}
       </div>
-
       <div className="info">
         <h3 id="message">{message}</h3>
       </div>
-
       <div id="keypad">
         <button id="left" onClick={ilerle}>
           SOL
@@ -101,16 +123,15 @@ export default function AppFunctional(props) {
           reset
         </button>
       </div>
-
       <form onSubmit={onSubmit}>
         <input
           id="email"
           type="email"
           placeholder="email girin"
-          value={email}
           onChange={onChange}
-        />
-        <input id="submit" type="submit" value="Gönder" />
+          value={email}
+        ></input>
+        <input id="submit" type="submit"></input>
       </form>
     </div>
   );
